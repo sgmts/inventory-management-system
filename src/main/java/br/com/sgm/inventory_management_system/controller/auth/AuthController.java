115a0822d2987c5adec766e46cedb4d330798caf
sgmartinss <sgmartins777@gmail.com>
2025-01-27 18:37:42 -0300

    docs: Swagger Adicionado ao AuthController

--- src/main/java/br/com/sgm/inventory_management_system/controller/auth/AuthController.java
+++ src/main/java/br/com/sgm/inventory_management_system/controller/auth/AuthController.java
@@ -5,8 +5,15 @@ import br.com.sgm.inventory_management_system.controller.service.UserService;
 import br.com.sgm.inventory_management_system.dto.JwtResponseDTO;
 import br.com.sgm.inventory_management_system.dto.LoginRequestDTO;
 import br.com.sgm.inventory_management_system.dto.UserDTO;
+import br.com.sgm.inventory_management_system.security.SecurityConfig;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@Tag(name = "Inventory Management System", description = "Sistema de Gestão de Estoque Seguro")
+@SecurityRequirement(name = SecurityConfig.SECURITY)
 public class AuthController {
 
     private final UserService userService;
@@ -24,15 +33,27 @@ public class AuthController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+    @Operation(summary = "Registra Usuario", description = "Endpoint responsável por Registrar o Usuario no sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Usuario registrado com sucesso."),
+            @ApiResponse(responseCode = "403",description = "Campo invalido ou nulo."),
+            @ApiResponse(responseCode = "500",description = "Erro Interno do servidor."),
+    })
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserDTO userDTO) {
         UserDTO registeredUser = userService.registerUser(userDTO);
-        return ResponseEntity.ok("Usuário registrado com sucesso: " + registeredUser.getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login no sistema", description = "Endpoint responsável por realizar o login no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Login realizado com sucesso."),
+            @ApiResponse(responseCode = "403",description = "Email e/ou senha invalido."),
+            @ApiResponse(responseCode = "500",description = "Erro Interno do servidor."),
+    })
     public ResponseEntity<JwtResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequest) {
         String token = authService.authenticate(loginRequest);
-        return ResponseEntity.ok(new JwtResponseDTO(token));
+        return ResponseEntity.status(HttpStatus.OK).body(new JwtResponseDTO(token));
     }
 
 }
--- src/main/java/br/com/sgm/inventory_management_system/security/SecurityConfig.java
+++ src/main/java/br/com/sgm/inventory_management_system/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package br.com.sgm.inventory_management_system.security;
 
 import br.com.sgm.inventory_management_system.filter.JwtAuthenticationFilter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +18,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity // Habilita o uso de @PreAuthorize e @PostAuthorize
+@SecurityScheme(name = SecurityConfig.SECURITY,type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SecurityConfig {
+
+    public static final String SECURITY = "bearerAuth";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // Endpoints públicos
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"api/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"api/auth/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**","swagger-ui/**","swagger-ui.html").permitAll()
                         .requestMatchers("/api/users/**").hasRole("ADMIN") // Apenas ADMIN pode acessar
                         .anyRequest().authenticated() // Qualquer outro endpoint requer autenticação
                 )
